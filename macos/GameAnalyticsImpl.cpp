4b5b38e462e1e64d5162687e80bd0f42356eb764
thai.phi <thai.phi@indigames.net>
2021-06-14 12:14:17 +0700

    Fix macOS compile

--- macos/GameAnalyticsImpl.cpp
+++ macos/GameAnalyticsImpl.cpp
@@ -1,11 +1,14 @@
 #include "GameAnalyticsImpl.h"
 #include "GAnalytics.h"
 
-#include "GameAnalytics.h"
-using namespace gameanalytics;
+#ifdef BUILD_EXTENSION
+    #include "GameAnalytics.h"
+    using namespace gameanalytics;
+#endif
 
 void GameAnalyticsImpl::Init(const char* version, const char* game_key, const char* secret_key, bool debug)
 {
+#ifdef BUILD_EXTENSION
 	if (debug)
 	{
 		GameAnalytics::setEnabledInfoLog(true);
@@ -13,29 +16,40 @@ void GameAnalyticsImpl::Init(const char* version, const char* game_key, const ch
 	}
 	GameAnalytics::configureBuild(version);
 	GameAnalytics::initialize(game_key, secret_key);
+#endif
 }
 
 void GameAnalyticsImpl::Release()
 {
+#ifdef BUILD_EXTENSION
 	GameAnalytics::onQuit();
+#endif
 }
 
 void GameAnalyticsImpl::addProgressionEvent(int progressionStatus, const char* progression01, const char* progression02, const char* progression03)
 {
+#ifdef BUILD_EXTENSION
 	GameAnalytics::addProgressionEvent((EGAProgressionStatus)progressionStatus, progression01, progression02, progression03);
+#endif
 }
 
 void GameAnalyticsImpl::addProgressionEvent(int progressionStatus, const char* progression01, const char* progression02, const char* progression03, int score)
 {
+#ifdef BUILD_EXTENSION
 	GameAnalytics::addProgressionEvent((EGAProgressionStatus)progressionStatus, progression01, progression02, progression03, score);
+#endif
 }
 
 void GameAnalyticsImpl::addDesignEvent(const char* eventId)
 {
+#ifdef BUILD_EXTENSION
 	GameAnalytics::addDesignEvent(eventId);
+#endif
 }
 
 void GameAnalyticsImpl::addDesignEvent(const char* eventId, double value)
 {
+#ifdef BUILD_EXTENSION
 	GameAnalytics::addDesignEvent(eventId, value);
+#endif
 }
